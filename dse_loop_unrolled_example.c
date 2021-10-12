cb1a7c61c1550f271791a2dcf4f4bd2fd7d02d3b
Thuan Pham <thuan.pham@unimelb.edu.au>
2021-10-12 11:48:42 +1100

    Add examples for Dynamic Symbolic Execution

---
+++ dse_loop_unrolled_example.c
@@ -0,0 +1,63 @@
+//This program is a modification of an example published by ADALogics
+//https://github.com/AdaLogics/adacc/blob/master/examples/afl-sample.c
+
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int foo(char *arr, int count) {
+    int i = 0;
+
+    if (i < count) {
+      if (arr[i] == 'A' + i)
+        return i;
+      i++;
+    }
+
+    if (i < count) {
+      if (arr[i] == 'A' + i)
+        return i;
+      i++;
+    }
+
+    if (i < count) {
+      if (arr[i] == 'A' + i)
+        return i;
+      i++;
+    }
+
+    if (i < count) {
+      if (arr[i] == 'A' + i)
+        return i;
+      i++;
+    }
+
+    if (*(int*)(arr) != 0x4d4f4f42) {
+        return i;
+    }
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    // open file
+    FILE *f = fopen(argv[1], "rb");
+
+    // get file size
+    fseek(f, 0, SEEK_END);
+    long fsize = ftell(f);
+
+    // read file contents
+    fseek(f, 0, SEEK_SET);
+    char *string = (char*)malloc(fsize + 1);
+    fread(string, 1, fsize, f);
+    fclose(f);
+
+    // call into target
+    int retval = foo(string, fsize);
+
+    free(string);
+    return retval;
+}
